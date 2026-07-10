3fa506fab546786e94523862145770756560bd3f
Aayushi Pujara <aayupujara80@gmail.com>
2026-07-10 16:01:48 +0530

    Add files via upload

---
+++ u1p6_4.java
@@ -0,0 +1,24 @@
+//6.4
+import java.util.Scanner;
+	public class u1p6_4
+	{
+		public static void main(String args[])
+		{
+			Scanner scn = new Scanner(System.in);
+			
+			System.out.println("Enter the string:");
+			String Userstr=scn.nextLine();
+			int num=scn.nextInt();
+			scn.close();
+			
+			
+			int i;
+			 for ( i=1; i <= num; i++)
+			 {
+              System.out.println(Userstr);
+             }
+			
+			
+
+  }
+}
